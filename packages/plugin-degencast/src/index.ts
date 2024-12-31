003164cf379bbcdab3b78b3b56fceb99ee4ddb93
bufan <bufan@hotmail.com>
2024-12-31 17:51:15 +0800

    disable create token action

--- packages/plugin-degencast/src/index.ts
+++ packages/plugin-degencast/src/index.ts
@@ -1,12 +1,12 @@
 import { Plugin } from "@elizaos/core";
-import { createTokenAction } from "./actions/createToken.ts";
+// import { createTokenAction } from "./actions/createToken.ts";
 import { airdropAction } from "./actions/airdrop.ts";
 export * as actions from "./actions";
 
 export const degencastPlugin: Plugin = {
     name: "degencast",
     description: "Degencast Plugin for Eliza",
-    actions: [createTokenAction, airdropAction],
+    actions: [airdropAction],
     evaluators: [],
     providers: [],
 };
